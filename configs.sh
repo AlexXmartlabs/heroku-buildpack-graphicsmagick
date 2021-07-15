1122a18145512e838d82ba711044377b3d59bd65
AlexXmartlabs <77295889+AlexXmartlabs@users.noreply.github.com>
2021-07-15 03:33:36 -0300

    Update configs.sh

--- configs.sh
+++ configs.sh
@@ -1,6 +1,6 @@
 
 export GRAPHICS_MAGICK_VERSION="1.3.36"
-export GM_FORCE_REBUILD="true"
+export GM_FORCE_REBUILD="false"
 # heroku dropped support for cedar, but leaving for if/when they introduce a
 # new stack
 export STACK="heroku-20"
